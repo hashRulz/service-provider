ae014d1ec77621e1c0c672b9aaaf05ecd8b32a60
hashrulz <hashananu94@gmail.com>
2022-07-08 14:00:23 +0530

    user authentication and login

---
+++ back-end/pune/src/main/java/com/mbaro/pune/config/AuthenticationBean.java
@@ -0,0 +1,25 @@
+package com.mbaro.pune.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class AuthenticationBean implements WebMvcConfigurer {
+
+    private String message;
+
+    public AuthenticationBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("HelloWorldBean [message=%s]", message);
+    }
+}
